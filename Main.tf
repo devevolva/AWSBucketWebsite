69e5d14857566dbf7e6550605d6e46bfd14fca1f
john <john@devevolva.com>
2019-04-18 11:49:03 -0400

    Refactor devevolvaweb to web-bucket. Minor formatting, whitespace tweaks.

--- Main.tf
+++ Main.tf
@@ -4,16 +4,17 @@
 #
 ###############################################################################
 
+###############################################################################
 # REGION ######################################################################
 provider "aws" {
     region = "us-east-1"
 }
 
 
-
+###############################################################################
 # BUCKET ######################################################################
-resource "aws_s3_bucket" "devevolvaweb" {
-  bucket           = "devevolvaweb"
+resource "aws_s3_bucket" "web-bucket" {
+  bucket           = "web-bucket"
   acl              = "public-read"
   policy           = "${file("s3_website_policy.json")}"
 
@@ -28,72 +29,72 @@ resource "aws_s3_bucket" "devevolvaweb" {
 }
 
 
-
+###############################################################################
 # FILES #######################################################################
 resource "aws_s3_bucket_object" "indexHtml" {
-  bucket       = "devevolvaweb"
+  bucket       = "web-bucket"
   key          = "index.html"
   acl          = "public-read"
   source       = "website\\index.html"
   etag         = "${md5(file("website\\index.html"))}"
   content_type = "text/html"
-  depends_on   = ["aws_s3_bucket.devevolvaweb"] 
+  depends_on   = ["aws_s3_bucket.web-bucket"] 
 }
 
 resource "aws_s3_bucket_object" "errorHtml" {
-  bucket       = "devevolvaweb"
+  bucket       = "web-bucket"
   key          = "error.html"
   acl          = "public-read"
   source       = "website\\error.html"
   etag         = "${md5(file("website\\error.html"))}"
   content_type = "text/html"
-  depends_on   = ["aws_s3_bucket.devevolvaweb"]
+  depends_on   = ["aws_s3_bucket.web-bucket"]
 }
 
 resource "aws_s3_bucket_object" "mainCss" {
-  bucket       = "devevolvaweb"
+  bucket       = "web-bucket"
   key          = "main.css"
   acl          = "public-read"
   source       = "website\\main.css"
   etag         = "${md5(file("website\\main.css"))}"
   content_type = "text/css"
-  depends_on   = ["aws_s3_bucket.devevolvaweb"]
+  depends_on   = ["aws_s3_bucket.web-bucket"]
 }
 
 resource "aws_s3_bucket_object" "normalizeCss" {
-  bucket       = "devevolvaweb"
+  bucket       = "web-bucket"
   key          = "normalize.css"
   acl          = "public-read"
   source       = "website\\normalize.css"
   etag         = "${md5(file("website\\normalize.css"))}"
   content_type = "text/css"
-  depends_on   = ["aws_s3_bucket.devevolvaweb"]
+  depends_on   = ["aws_s3_bucket.web-bucket"]
 }
 
 resource "aws_s3_bucket_object" "scriptsJs" {
-  bucket       = "devevolvaweb"
+  bucket       = "web-bucket"
   key          = "scripts.js"
   acl          = "public-read"
   source       = "website\\scripts.js"
   etag         = "${md5(file("website\\scripts.js"))}"
   content_type = "text/javascript"
-  depends_on   = ["aws_s3_bucket.devevolvaweb"]
+  depends_on   = ["aws_s3_bucket.web-bucket"]
 }
 
 
-
+###############################################################################
 # OTUPUTS #####################################################################
 output "webBucketARN" {
-      value = "${aws_s3_bucket.devevolvaweb.arn}"
+      value = "${aws_s3_bucket.web-bucket.arn}"
   }
 output "webBucketName" {
-      value = "${aws_s3_bucket.devevolvaweb.bucket}"
+      value = "${aws_s3_bucket.web-bucket.bucket}"
   }
 
 output "webBucketId" {
-      value = "${aws_s3_bucket.devevolvaweb.id}"
+      value = "${aws_s3_bucket.web-bucket.id}"
   }
 
 output "webBucketURL" {
-      value = "${aws_s3_bucket.devevolvaweb.bucket}.s3-website-${aws_s3_bucket.devevolvaweb.region}.amazonaws.com"
+      value = "${aws_s3_bucket.web-bucket.bucket}.s3-website-${aws_s3_bucket.web-bucket.region}.amazonaws.com"
   }
